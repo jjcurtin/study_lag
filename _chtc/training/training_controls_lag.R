755f4c6f4a1e3cb70304978664439f7fc3172f2c
Kendra Wyant <kpaquette2@wisc.edu>
2025-06-05 10:23:06 -0500

    shap updates

--- _chtc/training/training_controls_lag.R
+++ _chtc/training/training_controls_lag.R
@@ -48,7 +48,7 @@ source("https://github.com/jjcurtin/lab_support/blob/main/format_path.R?raw=true
 # SET GLOBAL PARAMETERS--------------------
 study <- "lag"
 window <- "1day"
-lead <- 0
+lead <- 168
 version <- "v3" #feature version (v1 = 24 hour fence, v2 = 6 hour fence, v3 = 1day/24 hour fence)
 algorithm <- "xgboost"
 model <- "strat_lh"
